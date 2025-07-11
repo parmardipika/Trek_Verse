8bb81263324d00e4974e5ab5d071c50805dc0e6b
Dipika Parmar <parmardipika61825@gmail.com>
2025-07-11 16:37:12 +0530

    Add

--- models/review.js
+++ models/review.js
@@ -2,15 +2,19 @@ const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
 const reviewSchema=new Schema({
-    comment: String,
+    
     rating:{
         type:Number,
         min:1,
         max:5,
     },
+     comment: {
+    type: String,
+    required: true,
+  },
     createdAt:{
         type:Date,
-        default:Date.now()
+        default:Date.now
     }
 });
 
